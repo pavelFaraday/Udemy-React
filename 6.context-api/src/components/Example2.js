4740df698cc46455dc14134eef417246dff1c601
pavelFaraday <devstudio.corp@gmail.com>
2023-01-13 13:29:39 +0400

    6. Context API - examples

--- 6.context-api/src/components/Example2.js
+++ 6.context-api/src/components/Example2.js
@@ -1,35 +1,38 @@
 import React, { useState, createContext, useContext } from "react";
 
+// 1. create context
 const AppContext = createContext(null);
 
 export const Parent = () => {
-  const [userName, setUserName] = useState("PedroTech");
+	const [userName, setUserName] = useState("PedroTech");
 
-  return (
-    <AppContext.Provider value={{ userName, setUserName }}>
-      <Child1 />
-      <Child2 />
-    </AppContext.Provider>
-  );
+	return (
+		// 2. Wrap with context provider, 3. assigne value={}
+		<AppContext.Provider value={{ userName, setUserName }}>
+			<Child1 />
+			<Child2 />
+		</AppContext.Provider>
+	);
 };
 
 export const Child1 = () => {
-  const { userName } = useContext(AppContext);
+	const { userName } = useContext(AppContext);
 
-  return <h1> {userName} </h1>;
+	return <h1> {userName} </h1>;
 };
 
 export const Child2 = () => {
-  const { setUserName } = useContext(AppContext);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setUserName("PedroTechnologies");
-        }}
-      >
-        Change Username
-      </button>
-    </div>
-  );
+	// 4. use context
+	const { setUserName } = useContext(AppContext);
+	return (
+		<div>
+			<button
+				onClick={() => {
+					setUserName("PedroTechnologies");
+				}}
+			>
+				Change Username
+			</button>
+		</div>
+	);
 };
--- 6.context-api/src/components/Example3.js
+++ 6.context-api/src/components/Example3.js
@@ -1,58 +1,65 @@
 import React, { useState, createContext, useContext } from "react";
 
+// 1. create context
 const AppContext = createContext(null);
 
 export const Parent = () => {
-  const [userName, setUserName] = useState("PedroTech");
-  const [count, setCount] = useState(0);
+	const [userName, setUserName] = useState("PedroTech");
+	const [count, setCount] = useState(0);
 
-  return (
-    <AppContext.Provider value={{ userName, setUserName, count, setCount }}>
-      <Child1 />
-      <Child2 />
-      <Child3 />
-      <Child4 />
-    </AppContext.Provider>
-  );
+	return (
+		// 2. wrap parent with <AppContext.Provider>
+		// 3. add value={}
+		<AppContext.Provider value={{ userName, setUserName, count, setCount }}>
+			<Child1 />
+			<Child2 />
+			<Child3 />
+			<Child4 />
+		</AppContext.Provider>
+	);
 };
 
 export const Child1 = () => {
-  const { userName } = useContext(AppContext);
+	// 4. use Context
+	const { userName } = useContext(AppContext);
 
-  return <h1> {userName} </h1>;
+	return <h1> {userName} </h1>;
 };
 
 export const Child2 = () => {
-  const { setUserName } = useContext(AppContext);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setUserName("PedroTechnologies");
-        }}
-      >
-        Change Username
-      </button>
-    </div>
-  );
+	// 4. use Context
+	const { setUserName } = useContext(AppContext);
+	return (
+		<div>
+			<button
+				onClick={() => {
+					setUserName("PedroTechnologies");
+				}}
+			>
+				Change Username
+			</button>
+		</div>
+	);
 };
 
 export const Child3 = () => {
-  const { count } = useContext(AppContext);
-  return <h1>{count}</h1>;
+	// 4. use Context
+	const { count } = useContext(AppContext);
+	return <h1>{count}</h1>;
 };
 
 export const Child4 = () => {
-  const { setCount } = useContext(AppContext);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setCount((c) => c + 1);
-        }}
-      >
-        Change Count
-      </button>
-    </div>
-  );
+	// 4. use Context
+	const { setCount } = useContext(AppContext);
+	return (
+		<div>
+			<button
+				onClick={() => {
+					setCount((c) => c + 1);
+				}}
+			>
+				Change Count
+			</button>
+		</div>
+	);
 };
