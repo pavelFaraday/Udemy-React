cd6fb79bddba21f9df8d2016a5184f649b608113
pavelFaraday <devstudio.corp@gmail.com>
2023-02-03 20:36:07 +0400

    add inputs

--- 9.http-requests/src/App.js
+++ 9.http-requests/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from "react";
-
+import AddMovie from "./components/AddMovie";
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
@@ -38,22 +38,26 @@ function App() {
 		fetchMoviesHandler();
 	}, [fetchMoviesHandler]);
 
-	let content = <p>Found no movies</p>;
+	function addMovieHandler(movie) {
+		console.log(movie);
+	}
 
+	let content = <p>Found no movies</p>;
 	if (movies.length > 0) {
 		content = <MoviesList movies={movies} />;
 	}
-
 	if (error) {
 		content = <p>{error}</p>;
 	}
-
 	if (isLoading) {
 		content = <p>Loading...</p>;
 	}
 
 	return (
 		<React.Fragment>
+			<section>
+				<AddMovie onAddMovie={addMovieHandler} />
+			</section>
 			<section>
 				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
 			</section>
