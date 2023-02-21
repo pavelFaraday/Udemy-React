2efebc967fab1efa77fc81f21474b24e405a7fc8
pavelFaraday <devstudio.corp@gmail.com>
2023-02-21 20:05:50 +0400

    272. Navigating between Pages with Links

--- 14.SPA/src/pages/Home.js
+++ 14.SPA/src/pages/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-	return <div>My Home Page</div>;
+	return (
+		<>
+			<h1>My Home Page</h1>
+			<p>
+				Go to <Link to="/products"> the list of products</Link>
+			</p>
+		</>
+	);
 };
 
 export default Home;
