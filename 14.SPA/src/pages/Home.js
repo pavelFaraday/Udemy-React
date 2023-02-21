f8e81992456d0954674ec897e4d1a4319d8fd6d5
pavelFaraday <devstudio.corp@gmail.com>
2023-02-21 21:37:16 +0400

    276. Navigating Programmatically (useNavigate)

--- 14.SPA/src/pages/Home.js
+++ 14.SPA/src/pages/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+	const navigate = useNavigate();
+	const navigateHandler = () => {
+		navigate("/products");
+	};
 	return (
 		<>
 			<h1>My Home Page</h1>
+			<button onClick={navigateHandler}>Navigate</button>
 		</>
 	);
 };
