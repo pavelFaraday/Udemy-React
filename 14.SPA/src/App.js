909c8e72b48a4da32f4766fc7394ae8737ad4496
pavelFaraday <devstudio.corp@gmail.com>
2023-02-21 20:54:57 +0400

    273. Layouts & Nested Routes (children, Outlet)

--- 14.SPA/src/App.js
+++ 14.SPA/src/App.js
@@ -1,10 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
-	{ path: "/", element: <Home /> },
-	{ path: "/products", element: <Products /> },
+	{
+		path: "/",
+		element: <RootLayout />,
+		children: [
+			{ path: "/", element: <Home /> },
+			{ path: "/products", element: <Products /> },
+		],
+	},
 ]);
 
 function App() {
--- 14.SPA/src/pages/Home.js
+++ 14.SPA/src/pages/Home.js
@@ -1,13 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Home = () => {
 	return (
 		<>
 			<h1>My Home Page</h1>
-			<p>
-				Go to <Link to="/products"> the list of products</Link>
-			</p>
 		</>
 	);
 };
